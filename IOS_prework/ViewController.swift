85f5f8cec7cfce53d2d8e3e33fb9bf0cec12eb27
Lesly Williams <82903572+Lwilliams002@users.noreply.github.com>
2023-08-25 00:31:14 -0400

    updated features
    Now you can change the text color of all labels

--- IOS_prework/ViewController.swift
+++ IOS_prework/ViewController.swift
@@ -11,23 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
-    @IBAction func changeBackgroundColor(_ sender: UIButton) {
-        func changeColor()-> UIColor{
-            
-            let red = CGFloat.random(in: 0...1);
-            let green = CGFloat.random(in: 0...1);
-            let blue = CGFloat.random(in: 0...1);
-            
-            return UIColor(red: red, green: green, blue: blue, alpha: 0.5);
-        }
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
         
-        let randomColor = changeColor();
-        view.backgroundColor = randomColor;
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
 
+    
+    @IBOutlet weak var myLabels1: UILabel!
+    @IBOutlet weak var myLabels2: UILabel!
+    @IBOutlet weak var myLabels3: UILabel!
+    
+    @IBAction func changeLabelColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        
+        let myLables = [ myLabels1, myLabels2, myLabels3]
+        
+        for label in myLables{
+            label?.textColor = randomColor
+        }
     }
     
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
 }
-
